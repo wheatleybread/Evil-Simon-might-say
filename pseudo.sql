2e30eddeced4bd9d182c29c3c6a885caf3f1e2fd
mrmoe24 <tarron.cooper@education.nsw.gov.au>
2024-08-19 09:31:31 +1000

    Update pseudo.sql

--- pseudo.sql
+++ pseudo.sql
@@ -1,1 +1,15 @@
 BEGIN
+WHILE True
+    y = random int 1-4
+    Flash corresponding LED from int
+    corresponding Piezo sound from int
+    ButtonPress = GetButton()
+    if ButtonPress = y
+        correct
+    else if 
+        incorrect
+    if correct
+        x += 1
+    else if 
+        ENDWHILE
+END
