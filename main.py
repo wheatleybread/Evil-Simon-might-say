fbea11c9b92133670d4ee2d6ab278193198e19fc
wheatleybread <ryan.wheatley253@education.nsw.gov.au>
2024-08-19 10:30:32 +1000

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,17 @@ y = 3
 list = []
 
 
+piezo.freq(2000)
+piezo.duty_u16(1000)
+
+
 for x in range(y):
     list.append(random.randint(1,4))
     sleep(0.5)
 
 print(list)
 print(list[0])
+
+while True:
+    value = potometer.read_u16()
+    print (f"Pot: {value}")
