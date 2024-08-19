ce63dbcfaf83c4de8fa79f75f9b8d3ffa5ba0610
mrmoe24 <tarron.cooper@education.nsw.gov.au>
2024-08-19 10:27:21 +1000

    rg
    jhsgf

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import random
 from time import sleep
+from machine import Pin, PWM, ADC
 
+redButton = Pin(17, Pin.IN)
+blueButton = Pin(19, Pin.IN)
+greenButton = Pin(20, Pin.IN)
+yellowButton = Pin(18, Pin.IN)
+piezo = PWM(Pin(14))
+potometer = ADC(Pin(26))
+redLed = Pin(11)
+blueLed = Pin(9)
+greenLed = Pin(10)
+yellowLed = Pin(12)
 
 y = 3
 
