8d27ec3ff1545da46423643736c567f22be12b8b
mrmoe24 <tarron.cooper@education.nsw.gov.au>
2024-08-26 14:44:10 +1000

    fgh

--- main.py
+++ main.py
@@ -19,30 +19,50 @@ yellowLed = Pin(13, Pin.OUT)
 choice = 0
 gamelist = []
 playerlist = []
-level = 1
+level = 3
+list = 0
 
 while True:
-    gamelist.append(randint(1,5))
+    
     for x in range(level):
-        choice = gamelist[level]
+        gamelist.append(randint(1,4))
+        sleep(0.5)
+        choice = gamelist[list]
         if choice == 1:
             redLed.on()
             sleep(0.5)
             redLed.off()
+            list += 1
         elif choice == 2:
             blueLed.on()
             sleep(0.5)
             blueLed.off()
+            list += 1
         elif choice == 3:
             greenLed.on()
             sleep(0.5)
             greenLed.off()
+            list += 1
         elif choice == 4:
             yellowLed.on()
             sleep(0.5)
             yellowLed.off()
-       
-    break
+            list += 1
+    break   
 
-    
+for x in range(level):
+    if redButton.value == True:
+        playerlist.append(1)
+        sleep(0.5)
+    elif blueButton.value == True:
+        playerlist.append(2)
+        sleep(0.5)
+    elif greenButton.value == True:
+        playerlist.append(3)
+        sleep(0.5)
+    elif yellowButton.value == True:
+        playerlist.append(4)
+        sleep(0.5)
 
+if playerlist == gamelist:
+    print('Yay!')
