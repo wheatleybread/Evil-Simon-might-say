ddd66c10f6e3f563fd04b09ab701e87085216cf8
wheatleybread <ryan.wheatley253@education.nsw.gov.au>
2024-08-29 12:53:16 +1000

    Simpsons Family
    Added Homer(class) and Bart (object) to the simpsons house (our code)

--- main.py
+++ main.py
@@ -3,131 +3,132 @@ from time import sleep
 from machine import Pin, PWM, ADC
 from piezoNotes import *
 
+class Homer:
 
-# Defining buttons
-redButton = Pin(2, Pin.IN)
-blueButton = Pin(0, Pin.IN)
-greenButton = Pin(3, Pin.IN)
-yellowButton = Pin(1, Pin.IN)
+    # Defining buttons
+    redButton = Pin(2, Pin.IN)
+    blueButton = Pin(0, Pin.IN)
+    greenButton = Pin(3, Pin.IN)
+    yellowButton = Pin(1, Pin.IN)
 
-#Defining Piezo
-piezo = PWM(Pin(20))
+    #Defining Piezo
+    piezo = PWM(Pin(20))
 
-#Defining lights
-redLed = Pin(14, Pin.OUT)
-blueLed = Pin(12, Pin.OUT)
-greenLed = Pin(15, Pin.OUT)
-yellowLed = Pin(13, Pin.OUT)
+    #Defining lights
+    redLed = Pin(14, Pin.OUT)
+    blueLed = Pin(12, Pin.OUT)
+    greenLed = Pin(15, Pin.OUT)
+    yellowLed = Pin(13, Pin.OUT)
 
+    #Basic variables
+    length = 0
+    choice = 0
+    playerlist = []
+    gamelist = []
+    level = 1
+    list = 0
 
-#Setting up the iezo
-def PlayNote(note, duration):
-    piezo.freq(notes[note])
-    piezo.duty_u16(1000)
-    sleep(duration)
-    piezo.duty_u16(0)
-
-
-#Basic variables
-length = 0
-choice = 0
-playerlist = []
-gamelist = []
-level = 1
-list = 0
-
-def sequencedisplay():
-    for x in range(level):
-            playerturn = False
-            sleep(0.5)
-            print(list)
-            choice = gamelist[list]
-            if choice == 1:
-                redLed.on()
-                PlayNote("C5", 0.4)
-                redLed.off()
-                list += 1
-            elif choice == 2:
-                blueLed.on()
-                PlayNote("C3", 0.4)
-                blueLed.off()
-                list += 1
-            elif choice == 3:
-                greenLed.on()
-                PlayNote("C6", 0.4)
-                greenLed.off()
-                list += 1
-            elif choice == 4:
-                yellowLed.on()
-                PlayNote("C4", 0.4)
-                yellowLed.off()
-                list += 1
-            playerturn = True
-
-
-def playertime():
-    for x in range(level):      
-            while playerturn == True:
-                if redButton.value() == True:
-                    playerlist.append(1)
-                    sleep(0.1)
-                    playerturn = False
-                elif blueButton.value() == True:
-                    playerlist.append(2)
-                    sleep(0.1)
-                    playerturn = False
-                elif greenButton.value() == True:
-                    playerlist.append(3)
-                    sleep(0.1)
-                    playerturn = False
-                elif yellowButton.value() == True:
-                    playerlist.append(4)
-                    sleep(0.1)
-                    playerturn = False
-            #if correct
-            if playerlist[length] == gamelist[length]:
-                    print('Yay!')
-                    PlayNote("E5", 0.25)
-            #if incorrect
-            else:
-                print ("Lost!")
-                PlayNote("B0", 0.5)
-                redLed.on()
-                blueLed.on()
-                greenLed.on()
-                yellowLed.on()
-                sleep(1)
-                redLed.off()
-                greenLed.off()
-                yellowLed.off()
-                blueLed.off()
-                print("DIE")
-                gamelist = []
-                level = 0
-                break
-            length += 1
-            playerturn = True
-
-#The game
-def game():
-    global length, level, gamelist, playerlist, list, choice, x
-
-    #randomising buttons
-    while True:
-        for x in range(1):
-        #adding random value to list
-            gamelist.append(randint(1,4))
-        #displaying input
-        sequencedisplay()
-
-        #taking player input
-        playertime()
-        #adding to level and resetting values
-        level += 1
-        length = 0
-        list = 0
-        playerlist = []
+    #Setting up the iezo
+    def PlayNote(self, note, duration):
+        self.piezo.freq(notes[note])
+        self.piezo.duty_u16(1000)
+        sleep(duration)
+        self.piezo.duty_u16(0)
 
 
 
-game()
+    def sequencedisplay(self):
+        for x in range(self.level):
+                self.playerturn = False
+                sleep(0.5)
+                print(self.list)
+                choice = self.gamelist[self.list]
+                if choice == 1:
+                    self.redLed.on()
+                    self.PlayNote("C5", 0.4)
+                    self.redLed.off()
+                    self.list += 1
+                elif choice == 2:
+                    self.blueLed.on()
+                    self.PlayNote("C3", 0.4)
+                    self.blueLed.off()
+                    self.list += 1
+                elif choice == 3:
+                    self.greenLed.on()
+                    self.PlayNote("C6", 0.4)
+                    self.greenLed.off()
+                    self.list += 1
+                elif choice == 4:
+                    self.yellowLed.on()
+                    self.PlayNote("C4", 0.4)
+                    self.yellowLed.off()
+                    self.list += 1
+                self.playerturn = True
+
+
+    def playertime(self):
+        for x in range(self.level):      
+                while self.playerturn == True:
+                    if self.redButton.value() == True:
+                        self.playerlist.append(1)
+                        sleep(0.1)
+                        self.playerturn = False
+                    elif self.blueButton.value() == True:
+                        self.playerlist.append(2)
+                        sleep(0.1)
+                        self.playerturn = False
+                    elif self.greenButton.value() == True:
+                        self.playerlist.append(3)
+                        sleep(0.1)
+                        self.playerturn = False
+                    elif self.yellowButton.value() == True:
+                        self.playerlist.append(4)
+                        sleep(0.1)
+                        self.playerturn = False
+                #if correct
+                if self.playerlist[self.length] == self.gamelist[self.length]:
+                        print('Yay!')
+                        self.PlayNote("E5", 0.25)
+                #if incorrect
+                else:
+                    print ("Lost!")
+                    self.PlayNote("B0", 0.5)
+                    self.redLed.on()
+                    self.blueLed.on()
+                    self.greenLed.on()
+                    self.yellowLed.on()
+                    sleep(1)
+                    self.redLed.off()
+                    self.greenLed.off()
+                    self.yellowLed.off()
+                    self.blueLed.off()
+                    print("DIE")
+                    self.gamelist = []
+                    self.level = 0
+                    break
+                self.length += 1
+                self.playerturn = True
+
+    #The game
+    def game(self):
+
+        #randomising buttons
+        while True:
+            for x in range(1):
+            #adding random value to list
+                self.gamelist.append(randint(1,4))
+            #displaying input
+            self.sequencedisplay()
+
+            #taking player input
+            self.playertime()
+            #adding to level and resetting values
+            self.level += 1
+            self.length = 0
+            self.list = 0
+            self.playerlist = []
+
+Bart = Homer()
+
+
+Bart.game()
