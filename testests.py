30c88760b46528ac19356d57cca26a48d2a8c526
mrmoe24 <tarron.cooper@education.nsw.gov.au>
2024-08-29 13:12:28 +1000

    THE FINAL THINGY

---
+++ testests.py
@@ -0,0 +1,24 @@
+from main import Homer
+
+bart = Homer()
+bart.level = 3
+bart.list = 0
+bart.gamelist = [1,2,3,4]
+bart.playerturn = False
+bart.playerlist = []
+bart.length = 0
+
+
+print ("Testing PlayNote")
+bart.PlayNote("E5", 0.27)
+
+
+print ("Testing SequencDisplay")
+bart.sequencedisplay()
+
+print ("Testing PlayerTimer")
+bart.playertime()
+
+print(' Testing game')
+
+bart.game()ame()
